a29e5fd3abf789f25c2dcb7cb0e9f678150603df
hong-michael <60994254+hong-michael@users.noreply.github.com>
2025-01-09 23:20:59 -0800

    Refactor gear_ratios.py

--- gear_ratios.py
+++ gear_ratios.py
@@ -8,11 +8,13 @@ for i in input:
     max_colors = {"red": 0, "green": 0, "blue": 0}
     ID, rest = i.split(":")
     games = rest.split(";")
+    
     for g in games:
         draws = g.split(",")
         for d in draws:
             num_colors = d.split()
             max_colors[num_colors[1]] = max(int(num_colors[0]), max_colors[num_colors[1]])
+            
     result += max_colors["red"] * max_colors["green"] * max_colors["blue"]
 
 print(result)
